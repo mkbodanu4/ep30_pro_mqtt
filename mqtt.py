4886708b3562c19ce636c7ddba31d065d329ac99
mkbodanu4 <mailmanbo@gmail.com>
2022-11-21 16:15:20 +0200

    Battery Level optimization

--- mqtt.py
+++ mqtt.py
@@ -512,14 +512,6 @@ while True:
 
         publish_single(topic=topic('output_power/state'), payload=str(output_power))
 
-    if battery_voltage is not None:
-        if float(battery_voltage) > float(configuration['invertor_config']['full_voltage']):
-            battery_level = 100
-        else:
-            battery_level = int((float(battery_voltage) - float(configuration['invertor_config']['empty_voltage'])) / (float(configuration['invertor_config']['full_voltage']) - float(configuration['invertor_config']['empty_voltage'])) * 100)
-
-        publish_single(topic=topic('battery_level/state'), payload=str(battery_level))
-
     if battery_voltage is not None and charging_current is not None:
         if is_charging == "ON":
             input_power = int(float(battery_voltage) * float(charging_current))
@@ -527,3 +519,16 @@ while True:
             input_power = 0
 
         publish_single(topic=topic('input_power/state'), payload=str(input_power))
+
+        if is_charging == "ON" and charging_current > float(configuration['charge_config']['float_current']):
+            if float(battery_voltage) > float(configuration['charge_config']['full_voltage']):
+                battery_level = 100
+            else:
+                battery_level = int((float(battery_voltage) - float(configuration['charge_config']['empty_voltage'])) / (float(configuration['charge_config']['full_voltage']) - float(configuration['charge_config']['empty_voltage'])) * 100)
+        else:
+            if float(battery_voltage) > float(configuration['discharge_config']['full_voltage']):
+                battery_level = 100
+            else:
+                battery_level = int((float(battery_voltage) - float(configuration['discharge_config']['empty_voltage'])) / (float(configuration['discharge_config']['full_voltage']) - float(configuration['discharge_config']['empty_voltage'])) * 100)
+
+        publish_single(topic=topic('battery_level/state'), payload=str(battery_level))
