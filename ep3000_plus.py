e1ffe89ee18118b6c1ace23a1090445648c0e672
Bohdan Manko <bohdan@manko.pro>
2025-08-02 17:03:19 +0300

    Minor fix

--- ep3000_plus.py
+++ ep3000_plus.py
@@ -431,7 +431,7 @@ while True:
     if configuration['backend'] == 'influx':
         metric = Point('Modbus').tag('Device', 'EP3000')
 
-    result = client.read_holding_registers(address=address, count=length, slave=slave_id)
+    result = client.read_holding_registers(address=address, count=length, device_id=slave_id)
     timestamp = time_ns()
 
     for sensor in sensors:
